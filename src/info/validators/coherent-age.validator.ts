25e2a96c04518a3c7a9d95aff61ea751ffb4e381
Rokas Stasiulis <rokastas@gmail.com>
2024-05-10 19:10:26 +0200

    added all the new validations for the second page

---
+++ src/info/validators/coherent-age.validator.ts
@@ -0,0 +1,25 @@
+import {
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+  ValidationArguments,
+} from 'class-validator';
+
+@ValidatorConstraint({ name: 'coherentAge', async: false })
+export class CoherentAgeValidator implements ValidatorConstraintInterface {
+  validate(dateOfBirth: string, args: ValidationArguments) {
+    const age = args.object['age'];
+    if (!dateOfBirth) return true;
+    const dob = new Date(dateOfBirth);
+    const today = new Date();
+    const calculatedAge = today.getFullYear() - dob.getFullYear();
+    const monthDiff = today.getMonth() - dob.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < dob.getDate())) {
+      return calculatedAge === age - 1;
+    }
+    return calculatedAge === age;
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return `The date of birth is not coherent with the provided age.`;
+  }
+}
